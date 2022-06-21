891e9833441d187cf8536854d2ede95b379baecc
Weiss Bai <whitesgr03@gmail.com>
2022-06-21 08:44:23 +0800

    Update script.js
    add event mouseover and mouseout to change color

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 
 
+
+function changeColor() {
+    const originColor = getComputedStyle(this)['backgroundColor']
+
+    if (originColor === 'rgb(0, 0, 0)') {
+        this.style['background-color'] = 'rgb(255, 255, 255)';
+    } else {
+        this.style['background-color'] = 'rgb(0, 0, 0)';
+    }
+}
+
+
+
 for (let i = 0; i < 16; i++) {
-    const wrap = document.createElement('div')
-    wrap.classList = 'wrap'
+    const wrap = document.createElement('div');
+
+    wrap.classList = 'wrap';
+
     document.body.appendChild(wrap);
 
     for (let i = 0; i < 16; i++) {
-        const div = document.createElement('div')
-        div.classList = 'square'
-        wrap.appendChild(div);
+        const square = document.createElement('div');
+
+        square.classList = 'square';
+
+        wrap.appendChild(square);
+
+        square.addEventListener('mouseover', changeColor);
+        square.addEventListener('mouseout', changeColor);
     }
+
+    
 }
 
